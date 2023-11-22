772360591f99c2821793b26110b71eef50c62b18
Jim Vickery <jimrv8@yahoo.com>
2023-11-21 21:20:14 -0600

    testing current_user

--- lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
+++ lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
@@ -216,13 +216,13 @@ module Ransack
                               .each_with_object({}) { |r, h| h[r.foreign_key.to_sym] = r.class_name }
 
           # Don't show 'id' column for base model
-          next nil if ((base.blank? && column == 'id') || (column.to_s.start_with?("cf") || column.to_s.start_with?("unsubscribe")))
-          # next nil if (base.blank? && column == 'id') || column.to_s.start_with?("unsubscribe")
+          # next nil if ((base.blank? && column == 'id') || (column.to_s.start_with?("cf") || column.to_s.start_with?("unsubscribe")))
+          next nil if (base.blank? && column == 'id') 
           
           # Testing this code
-          # if $ransack_ui_current_user != 1
-          #   next nil if (column.to_s.start_with?("cf"))
-          # end
+          if @current_user != 1
+            next nil if (column.to_s.start_with?("cf")) || (column.to_s.start_with?("unsubscribe"))
+          end
 
           attribute = attr_from_base_and_column(base, column)
           attribute_label = Translate.attribute(attribute, context: object.context)
