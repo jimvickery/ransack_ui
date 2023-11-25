62fb83af5da18b6f11fa6eec931985307d221019
Jim Vickery <jimrv8@yahoo.com>
2023-11-24 23:33:56 -0600

    added test code

--- lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
+++ lib/ransack_ui/ransack_overrides/helpers/form_builder.rb
@@ -216,7 +216,13 @@ module Ransack
                               .each_with_object({}) { |r, h| h[r.foreign_key.to_sym] = r.class_name }
 
           # Don't show 'id' column for base model
-          next nil if ((base.blank? && column == 'id') || (column.to_s.start_with?("cf") || column.to_s.start_with?("unsubscribe")))
+          next nil if ((base.blank? && coluumn == 'id') 
+          
+          if @current_user == 1
+            next nil if ((base.blank? && column == 'id') || (column.to_s.start_with?("cf") || column.to_s.start_with?("unsubscribe")))
+          else
+            next nil if ((base.blank? && coluumn == 'id') 
+          end
           # next nil if (base.blank? && column == 'id') || column.to_s.start_with?("unsubscribe")
           
           # Testing this code
